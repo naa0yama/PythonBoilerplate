012375c5d1551e8965d5dab88bfd5fc6adcb9f73
naa0yama <9667078+naa0yama@users.noreply.github.com>
2023-01-13 15:48:26 +0900

    add

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,8 @@
 # To add a new markdown cell, type '# %% [markdown]'
 # %%
 """test top conftest."""
-
 # Standard Library
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), "src"))
